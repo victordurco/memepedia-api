4ae67ae825a82cb2f07a2f1285e3cf65f6d29fd0
victordurco <victordurco@gmail.com>
2021-11-30 21:19:17 -0300

    all unit tests for memeService done

--- tests/unit/memeService.test.js
+++ tests/unit/memeService.test.js
@@ -1,8 +1,8 @@
-import Filter from "bad-words";
-
 import * as memeService from "../../src/services/memeService.js";
 import * as memeRepository from "../../src/repository/memeRepository.js";
 import * as userRepository from "../../src/repository/userRepository.js";
+
+jest.mock("bad-words");
 
 describe("listMemes tests", () => {
     it("Should return No memes today! when limit = 0", async () => {
@@ -17,7 +17,7 @@ describe("listMemes tests", () => {
             return [];
         });
 
-        const result = await memeService.listMemes(5);
+        const result = await memeService.listMemes(1);
         expect(result.message).toEqual("No memes today!");
         expect(result.data.length).toBe(0);
     });
@@ -33,8 +33,69 @@ describe("listMemes tests", () => {
             ];
         });
 
-        const result = await memeService.listMemes(5);
+        const result = await memeService.listMemes(1);
         expect(result.message).toEqual("List all memes");
+        expect(result.data.length).toBe(1);
+    });
+
+    it("Should return List all memes when limit = undefined and memes isn't empty", async () => {
+        jest.spyOn(memeRepository, "listMemes").mockImplementationOnce(() => {
+            return [
+                {
+                    url: "https://memegenerator.net/img/instances/62899729.jpg",
+                    text: "awesome meme",
+                    published_by: 1,
+                },
+            ];
+        });
+
+        const result = await memeService.listMemes(undefined);
+        expect(result.message).toEqual("List all memes");
+        expect(result.data.length).toBe(1);
+    });
+});
+
+describe("Insert meme tests", () => {
+    it("Should return No user! when token is invalid", async () => {
+        jest.spyOn(
+            userRepository,
+            "findUserByTokenSession"
+        ).mockImplementationOnce(() => {
+            return [];
+        });
+
+        const result = await memeService.insertMeme(
+            "invalidToken",
+            "https://memegenerator.net/img/instances/62899729.jpg",
+            "awesome meme"
+        );
+        expect(result.message).toEqual("No user!");
+        expect(result.data.length).toBe(0);
+    });
+
+    it("Should return inserted meme by user", async () => {
+        jest.spyOn(
+            userRepository,
+            "findUserByTokenSession"
+        ).mockImplementationOnce(() => {
+            return [{ id: 1 }];
+        });
+
+        jest.spyOn(memeRepository, "insertMeme").mockImplementationOnce(() => {
+            return [
+                {
+                    url: "https://memegenerator.net/img/instances/62899729.jpg",
+                    text: "awesome meme",
+                    published_by: 1,
+                },
+            ];
+        });
+
+        const result = await memeService.insertMeme(
+            "validToken",
+            "https://memegenerator.net/img/instances/62899729.jpg",
+            "its a new meme"
+        );
         expect(result.data.length).toBe(1);
     });
 });
